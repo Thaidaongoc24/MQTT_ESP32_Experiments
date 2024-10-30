077e0775d9c4e772893443fbea617aec048172da
thaidaongoc24 <thaidaongoc24@gmail.com>
2024-10-31 01:42:36 +0700

    hide secrets

--- WiFi_Disconnect_QoS0_Publishing/src/secrets/mqtt.h
+++
@@ -1,7 +0,0 @@
-namespace MQTT
-{
-    const char *username = "thaidn24";
-    const char *password = "123123";
-    const char *broker = "ef52bcb9a2654471a549247e267f5bff.s1.eu.hivemq.cloud";
-    uint16_t port = 8883;
-}
--- WiFi_Disconnect_QoS0_Publishing/src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-namespace WiFiSecrets
-{
-    const char *ssid = "Binhngungok";
-    const char *pass = "Binhnguvl";
-    const char *echo_topic = "esp32/echo_test";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;
-    uint16_t socketTimeout = 5;
-}
