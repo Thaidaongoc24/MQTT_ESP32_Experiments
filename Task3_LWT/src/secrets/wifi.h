23b89be84f604ca1d08f9acf7ccc8d41eb88ceb6
ThaisDN <92461962+Thaidaongoc24@users.noreply.github.com>
2024-10-31 15:44:23 +0700

    Delete Task3_LWT/src/secrets directory

--- Task3_LWT/src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-namespace WiFiSecrets
-{
-    const char *ssid = "MrThai";
-    const char *pass = "12345678";
-    const char *echo_topic = "esp32/echo_test";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;
-    uint16_t socketTimeout = 5;
-}
